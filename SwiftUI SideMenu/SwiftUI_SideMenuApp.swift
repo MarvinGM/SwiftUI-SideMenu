5619a6a930fd08e6c34a8a6c7fcdf8ad5a0b3dcd
MarvinGM <marvinmahilum091197@gmail.com>
2021-07-10 13:35:16 +0800

    Initial Commit

---
+++ SwiftUI SideMenu/SwiftUI_SideMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_SideMenuApp.swift
+//  SwiftUI SideMenu
+//
+//  Created by Amben on 7/10/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_SideMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
